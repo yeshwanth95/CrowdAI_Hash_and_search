5a48a7ea126fb97220fa5ff7455f42a1449076e7
yeshwanth95 <staryesh@gmail.com>
2023-04-05 12:14:05 +0300

    corrected filenames

--- compare_hashes.py
+++ compare_hashes.py
@@ -1,9 +1,8 @@
 import json
 import os
-import imagehash
 import argparse
 from tqdm import tqdm
-import multiprocessing
+import numpy as np
 
 
 parser = argparse.ArgumentParser(
@@ -16,48 +15,42 @@ parser.add_argument('--output_filename', required=True)
 args = parser.parse_args()
 
 
-haystack_hashes_path = args.haystack_hashfile
-needles_hashes_path = args.needles_hashfile
-
-with open(haystack_hashes_path) as f:
-    haystack_hashes = json.loads(f.read())
-with open(needles_hashes_path) as f:
-    needles_hashes = json.loads(f.read())
-
-
 def duplicates_per_image(im_name, im_hash, duplicate_images: list):
     _result = {}
-    _result["val_image_name"] = im_name
-    _result["val_image_hash"] = im_hash
+    _result["train_image_name"] = im_name
+    _result["train_image_hash"] = im_hash
     _result["duplicates_in_train"] = duplicate_images
 
     return _result
 
 
-def compare_single_needle_in_haystack(needle):
-        duplicates = []
-        v_name = needle['image_name']
-        v_hash = imagehash.hex_to_hash(needle['hash'])
-        for sample in haystack_hashes:
-            t_name = sample['image_name']
-            t_hash = imagehash.hex_to_hash(sample['hash'])
-            if t_hash == v_hash:
-                duplicates.append(sample)
-
-        return [v_name, str(v_hash), duplicates]
-
-
 def main(args):
+    haystack_hashes_path = args.haystack_hashfile
+    needles_hashes_path = args.needles_hashfile
 
-    duplicates_results = []
+    with open(haystack_hashes_path) as f:
+        haystack_hashes = json.loads(f.read())
+    with open(needles_hashes_path) as f:
+        needles_hashes = json.loads(f.read())
+    haystack_names_array, haystack_hash_array = [], []
+    for i in range(len(haystack_hashes)):
+        haystack_names_array.append(haystack_hashes[i]["image_name"])
+        haystack_hash_array.append(haystack_hashes[i]["hash"])
+
+    haystack_names_array = np.array(haystack_names_array)
+    haystack_hash_array = np.array(haystack_hash_array)
+
     counter = 0
-    with multiprocessing.Pool() as pool:
-        for result in tqdm(pool.imap(compare_single_needle_in_haystack, needles_hashes), total=len(needles_hashes)):
-            duplicates_results.append(duplicates_per_image(result[0], result[1], result[2]))
+    duplicates_results = []
+    for needle in tqdm(needles_hashes):
+        v_name = needle['image_name']
+        v_hash = np.array(needle['hash'])
 
-    for n in duplicates_results:
-        if len(n['duplicates_in_train']) > 0:
+        duplicates = haystack_names_array[haystack_hash_array == v_hash].tolist()
+        if len(duplicates) > 0:
             counter += 1
+        duplicates_results.append(duplicates_per_image(v_name, str(v_hash), duplicates))
+
     print(f"{counter} images of {len(needles_hashes)} images in needles haves duplicates in haystack.")
 
     with open(os.path.join(args.output_filename), "w") as f:
